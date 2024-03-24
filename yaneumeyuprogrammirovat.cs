749c74cb54ad08f925611b0644553567fcdb849b
AuroranFelix <126792271+AuroranFelix@users.noreply.github.com>
2024-03-24 15:22:31 +0300

    Add files via upload

---
+++ yaneumeyuprogrammirovat.cs
@@ -0,0 +1,17 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class yaneumeyuprogrammirovat : MonoBehaviour
+{
+    // Start is called before the first frame update
+    void Start()
+    {
+        print("crickets"); //окда? 
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+    }
+}
